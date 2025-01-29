a264aa77f7ba231a031caf8e92ca284e97e9db87
InakiSebastian <inakicabrerao@gmail.com>
2025-01-30 00:49:23 +0100

    Commit: Monitor Corregido

--- src/Controller/ActivityTypeController.php
+++ src/Controller/ActivityTypeController.php
@@ -7,12 +7,11 @@ use Symfony\Component\HttpFoundation\JsonResponse;
 use Symfony\Component\Routing\Attribute\Route;
 use App\Services\ActivityTypeService;
 
-#[Route('/activity-types')]
 class ActivityTypeController extends AbstractController
 {
     public function __construct(private ActivityTypeService $typeService) {}
 
-    #[Route('', name: 'get_activity_types', methods: ['GET'])]
+    #[Route('/activity-types', name: 'get_activity_types', methods: ['GET'])]
     public function getAll(): JsonResponse
     {
         $types = $this->typeService->findAll();
---
+++ src/Controller/MonitorController.php
@@ -0,0 +1,39 @@
+<?php
+
+namespace App\Controller;
+
+use App\Model\MonitorNewDto;
+use App\Services\MonitorsService;
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\JsonResponse;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\HttpKernel\Attribute\MapRequestPayload;
+use Symfony\Component\Routing\Attribute\Route;
+
+final class MonitorController extends AbstractController
+{
+
+    public function __construct(private MonitorsService $monitorsService)
+    {
+        
+    }
+
+    #[Route('/monitors', name: 'getMonitor',methods:['GET'])]
+    public function index(): JsonResponse
+    {
+        return $this->json($this->monitorsService->getMonitors());
+    }
+
+
+
+    #[Route('/monitors', name: 'addMonitor',methods:['POST'])]
+    public function addMonitor(#[MapRequestPayload(acceptFormat:'json',validationFailedStatusCode:Response::HTTP_NOT_FOUND)]MonitorNewDto $monitorNewDto): JsonResponse
+    {
+
+        $monitorCreado = $this->monitorsService->crearMonitors($monitorNewDto);
+        return $this->json($monitorCreado);
+    }
+
+
+
+}
