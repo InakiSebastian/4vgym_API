ea62f99393282629e153dc937a66613bf37a0f15
Efontser <149145102+Efontser@users.noreply.github.com>
2025-01-30 01:28:56 +0100

    Update ActivityNewDTO.php

--- src/Model/ActivityNewDTO.php
+++ src/Model/ActivityNewDTO.php
@@ -9,18 +9,19 @@ class ActivityNewDTO
 {
     public function __construct(
         public ?int $id,
-        #[Assert\NotBlank] 
+
+        #[Assert\NotBlank(message: "The activity type ID is required.")]
         public int $activity_type_id,
 
-        #[Assert\Count(min: 1)]
+        #[Assert\Count(min: 1, minMessage: "At least one monitor is required.")]
         public array $monitors_id,
 
-        #[Assert\NotBlank]
-        #[Assert\Type("string")]
+        #[Assert\NotBlank(message: "The start date is required.")]
+        #[Assert\Type(type: "string", message: "The start date must be a valid string.")]
         public string $date_start,
 
-        #[Assert\NotBlank]
-        #[Assert\Type("string")]
+        #[Assert\NotBlank(message: "The end date is required.")]
+        #[Assert\Type(type: "string", message: "The end date must be a valid string.")]
         public string $date_end
     ) {}
 }
